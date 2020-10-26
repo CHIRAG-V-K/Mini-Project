0b16e9ad0685e5bcb9229e8801f54602d72e33a9
Chirag.V.K <69964444+CHIRAG-V-K@users.noreply.github.com>
2020-10-27 02:22:00 +0530

    Home-page-constructed
    default page till we select the topic.

---
+++ home.js
@@ -0,0 +1,7 @@
+// gsap declaration
+const timeLine= gsap.timeline({defaults:{ease: 'power1.out'} });
+
+timeLine.to('.text',{y:"0%", duration: 1, stagger: 0.5});
+// timeLine.to('tag/class/id name',{css})
+timeLine.to('.slider',{y:"-100%", duration: 1.5, delay: 0.5});
+timeLine.to('.intro',{y:"-100%", duration: 1, delay: 0.5},"-=1.7");
