438c06d253a47adeb82d83fa94b87f25a72d51fb
CHIRAG-V-K <chirag.v.k2@gmail.com>
2020-10-30 14:58:11 +0530

    login page created

--- home.js
+++ home.js
@@ -5,3 +5,4 @@ timeLine.to('.text',{y:"0%", duration: 1, stagger: 0.5});
 // timeLine.to('tag/class/id name',{css})
 timeLine.to('.slider',{y:"-100%", duration: 1.5, delay: 0.5});
 timeLine.to('.intro',{y:"-100%", duration: 1, delay: 0.5},"-=1.7");
+
